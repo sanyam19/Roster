7e001fbf21823d5e9f64b80d8f01783eb470dc31
Sanyam Gupta <sanyamgupta19@gmail.com>
2021-01-11 20:13:37 +0530

    set up the ejs, views and use the middlewares

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const express = require('express');
 /* our express server will be running on this port*/
 const port = 8000;
 
+/* we need path for the views */
+/* path is inbuilt module to node, so we did`nt require npm install */
+const path = require('path');
+
 /* firing-up the express so as to get all the functionalities in the app */
 const app = express()
+
+/* telling the expresss that our viewing engine/ template engine will be ejs */
+app.set('view engine', 'ejs');
+
+/* placing our views in the views folder */
+/* instead of hard coded path, we can use current directory name */
+app.set('views', path.join(__dirname,'views'));
+
+/* middleware */
+/* since our url encoded also wants to read query params, so we`ll pass extended is true */
+app.use(express.urlencoded({
+    'extended': true
+}));
+/* assets is the folder which will contain static files css, js , images */
+app.use(express.static('assets'));
 
 /* now we need to run the server */
 app.listen(port, function(err){
